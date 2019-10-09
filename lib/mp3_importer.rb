c294c261dda93a6b06564e38260d67c2d79ea08c
Emil Andreasyan <emil.andreasyan@yahoo.com>
2019-10-09 07:01:48 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,8 +1,9 @@
 class MP3Importer
   
-  def initialize
-    @test_music_path
+  def initialize(test_music_path)
+    @music_importer.path = test_music_path
   end
+  
     def import(list_of_filenames)
       list_of_filenames.each {|filename| Song.new_by_filename(filename)}
     end
