7ce500372ec433d3080db2234068cc45162818af
Emil Andreasyan <emil.andreasyan@yahoo.com>
2019-10-09 07:22:36 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -20,7 +20,11 @@ class Song
   end
   
   def artist_name=(name)
-    
+    if (self.artist.nil?)
+      self.artist = Artist.new(name)
+    else
+      self.artist.name = name
+    end
   end
 end
 
