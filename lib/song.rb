2c9ae114a87d82bfd998234a14f49b9c4e5fdc52
Emil Andreasyan <emil.andreasyan@yahoo.com>
2019-10-09 07:18:12 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,7 +14,9 @@ class Song
   
   def self.new_by_filename(filename)
     song = self.new(filename)
-    song.name = filename.split(" - ")[1]
+    song.artist = artist
+    song
+    #song.name = filename.split(" - ")[1]
   end
 end
 
