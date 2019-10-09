b774d057b6310ad9f0a40b449e742ce00cbf0548
Emil Andreasyan <emil.andreasyan@yahoo.com>
2019-10-09 06:43:13 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -5,4 +5,4 @@ class MP3Importer
 end
 
 mp3 = MP3Importer.new
-mp3.import([Action Bronson - Larry Csonka - indie.mp3], )
+mp3.import(["Action Bronson - Larry Csonka - indie.mp3"], ["Real Estate - Green Aisles - country.mp3"], ["Real Estate - It's real hip-hop.mp3"], ["Thundercat - For love I come.mp3"])
